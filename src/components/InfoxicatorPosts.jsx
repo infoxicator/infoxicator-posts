1f4458bfcb0e53e9bc9fe2dd1f3c6393e460ffd3
infoxicator <ruben@infoxication.net>
2020-06-08 23:25:35 +0100

    feat(iguazu): add iguazu-rest

--- src/components/InfoxicatorPosts.jsx
+++ src/components/InfoxicatorPosts.jsx
@@ -3,14 +3,13 @@ import { configureIguazuSSR } from 'iguazu-holocron';
 import { connectAsync } from 'iguazu';
 import { withRouter } from '@americanexpress/one-app-router';
 import { compose } from 'redux';
-import { queryProcedureResult } from 'iguazu-rpc';
+import { queryCollection } from 'iguazu-rest';
 import PostsList from './PostsList';
 import LoadingSkeleton from './LoadingSkeleton';
-import reducer from '../duck';
 import './Posts.scss';
 
 const InfoxicatorPosts = ({
-  isLoading, loadedWithErrors, posts, postTitle, router: { location: { pathname } },
+  isLoading, loadedWithErrors, posts, postTitle, location: { pathname },
 }) => {
   if (isLoading()) return <LoadingSkeleton />;
   if (loadedWithErrors()) return <h1>Something went wrong...</h1>;
@@ -19,24 +18,18 @@ const InfoxicatorPosts = ({
   );
 };
 
-export const loadDataAsProps = ({ store: { dispatch } }) => {
-  const apiUrl = 'https://www.infoxication.net/wp-json/wp/v2/posts/';
-  return {
-    posts: () => dispatch(queryProcedureResult({ procedureName: 'readPosts', args: { api: apiUrl } })),
-  };
-};
-
-loadDataAsProps.ssr = true;
-InfoxicatorPosts.loadDataAsProps = loadDataAsProps;
-
-if (!global.BROWSER) {
-  InfoxicatorPosts.loadModuleData = configureIguazuSSR;
-}
-
 InfoxicatorPosts.holocron = {
   name: 'infoxicator-posts',
-  reducer,
+  loadModuleData: async ({ store, module, ownProps }) => {
+    await configureIguazuSSR({ store, module, ownProps });
+  },
 };
+
+export const loadDataAsProps = ({ store: { dispatch } }) => ({
+  posts: () => dispatch(queryCollection({ resource: 'posts', id: 'test' })),
+});
+
+loadDataAsProps.ssr = true;
 
 export default compose(
   connectAsync({ loadDataAsProps }),
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import ModuleContainer from './components/InfoxicatorPosts';
 
-import { configureIguazu } from './iguazu';
-
-configureIguazu();
-
 export default ModuleContainer;
